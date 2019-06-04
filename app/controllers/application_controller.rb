1fe9962b7c6e7f8959d9d66d79148d570f233388
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-04 12:23:28 -0400

    Completed lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,58 @@
+# frozen_string_literal: true
 
 require_relative '../../config/environment'
 
-class ApplicationController < Sinatra::Base
+set :method_override, true
 
+class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
   get '/' do
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  post '/articles' do
+    title = params['title']
+    content = params['content']
+    article = Article.create(title: title, content: content)
+    redirect "articles/#{article.id}"
+  end
+
+  get '/articles/:id' do
+    id = params[:id]
+    @article = Article.find(id)
+    erb :show
+  end
+
+  patch '/articles/:id' do
+    id = params[:id]
+    @article = Article.find(id)
+    title = params['title']
+    content = params['content']
+    @article.update(title: title, content: content)
+    redirect "articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    id = params[:id]
+    Article.delete(id)
+    redirect 'articles'
+  end
+
+  get '/articles/:id/edit' do
+    id = params[:id]
+    @article = Article.find(id)
+    erb :edit
   end
 end
