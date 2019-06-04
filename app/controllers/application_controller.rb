90a91cb1e3eaece43244f5f4d2d2634af5595601
Matt Williams <matt.seongjun@gmail.com>
2019-06-04 12:22:19 -0400

    Finish create and read

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,32 @@ require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+    end
 
-  get '/' do
-  end
+    get '/' do
+        redirect '/articles'
+    end
+
+    get '/articles' do
+        @articles = Article.all
+        erb :index
+    end
+
+    get '/articles/new' do
+        erb :new
+    end
+
+    post '/articles' do
+        new = Article.create(title: params[:title], content: params[:content])
+        redirect "/articles/#{new.id}"
+    end
+
+    get '/articles/:id' do
+        @article = Article.find(params[:id])
+        erb :show
+    end
+
 end
